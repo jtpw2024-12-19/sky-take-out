9a343e8d9e06311e09426af59465ffe3e7ad4504
Count <2671979643@qq.com>
2026-04-25 19:17:56 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -113,4 +113,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total, records);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        // update employee set status = ? where id = ?
+        // 下述传统方式
+//        Employee employee = new Employee();
+//        employee.setStatus(status);
+//        employee.setId(id);
+
+        // 下述采用 Employee 添加的Builder 模式
+        Employee employee = Employee.builder()
+                        .status(status)
+                                .id(id)
+                                        .build();
+
+        employeeMapper.update(employee);
+    }
+
 }
